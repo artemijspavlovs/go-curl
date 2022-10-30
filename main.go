4c8a09ae895e8c8fb698675c5bb8007049d40530
artemijspavlovs <artemijs.pavlovs@artpav.dev>
2022-10-30 14:04:20 +0200

    initial commit

---
+++ main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"fmt"
+	"github.com/alexflint/go-arg"
+	"io"
+	"log"
+	"net/http"
+	"regexp"
+	"sort"
+)
+
+type WebsiteResponse struct {
+	URL          string
+	ResponseSize int64
+}
+
+func main() {
+	var d []WebsiteResponse
+	c := make(chan WebsiteResponse)
+
+	// https://uibakery.io/regex-library/url
+
+	var args struct {
+		Urls []string `arg:"required"`
+	}
+	arg.MustParse(&args)
+
+	for _, url := range args.Urls {
+		go handleRequestsConcurrently(url, c)
+	}
+
+	for range args.Urls {
+		d = append(d, <-c)
+	}
+
+	sortResponses(d)
+
+	for _, r := range d {
+		fmt.Printf("%s\t%v bytes\n", r.URL, r.ResponseSize)
+	}
+}
+
+func handleRequestsConcurrently(url string, c chan<- WebsiteResponse) {
+	rHttpUrl := regexp.MustCompile("^https?:\\/\\/(?:www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&\\/=]*)$")
+	rUrl := regexp.MustCompile("(?:www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&\\/=]*)$")
+	if rUrl.MatchString(url) {
+		if !rHttpUrl.MatchString(url) {
+			url = "https://" + url
+		}
+		responseSize, err := handleRequest(url)
+
+		if err != nil {
+			log.Fatalf("Error handling request: %v", err)
+		}
+
+		c <- WebsiteResponse{
+			URL:          url,
+			ResponseSize: responseSize,
+		}
+	} else {
+		c <- WebsiteResponse{
+			URL:          url,
+			ResponseSize: 0,
+		}
+	}
+}
+
+// https://www.notebookcast.com/en/blog/golang-sort-array-struct
+func sortResponses(rl []WebsiteResponse) {
+	sort.Slice(rl, func(i, j int) bool { return rl[i].ResponseSize > rl[j].ResponseSize })
+}
+
+func handleRequest(url string) (int64, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	b, err := getResponseBodySize(resp)
+	if err != nil {
+		return 0, err
+	}
+	return b, nil
+}
+
+func getResponseBodySize(resp *http.Response) (int64, error) {
+	b, err := io.Copy(io.Discard, resp.Body)
+	if err != nil {
+		return 0, err
+	}
+	return b, nil
+}
